e96e8fbedf94cd3ee5b608076cfc6f54f6c55e0b
Melbin Mathew <melbinppmathewp@gmail.com>
2025-08-25 14:43:19 +0530

    3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Main from "./pages/main"
 import QRCode from "./pages/qr-code"
 import WebP from "./pages/webp-converter"
@@ -15,6 +15,8 @@ function App() {
           <Route path="/" element={ <Main /> } />
           <Route path="/qr-code" element={ <QRCode /> } />
           <Route path="/webp-converter" element={ <WebP /> } />
+
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Footer />
     </div>
